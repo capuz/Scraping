cf10ff71b0a20784b8f02f8e2b884607440c0694
fcapu <fcapu@FRANCESCO>
2022-10-29 11:57:38 -0300

    Agregar archivos de proyecto.

---
+++ Scraping/Program.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var url = "https://visualstudio.microsoft.com/es/";
+        HtmlWeb html = new();
+        HtmlDocument doc = html.Load(url);
+
+        var node = doc.DocumentNode.CssSelect(".hero-grid-text").Select(x => x.InnerText);
+       
+        foreach (var item in node) {
+            Console.WriteLine(item);
+        }
+      
+        Console.ReadKey();
+    }
+}
